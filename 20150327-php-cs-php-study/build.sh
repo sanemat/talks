39af817431679e189447856cc3b5cd44444c5d26
sanemat <o.gata.ken@gmail.com>
2015-03-27 15:56:05 +0900

    docs(phpcs): update

--- 20150327-php-cs-php-study/build.sh
+++ 20150327-php-cs-php-study/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # cp html5github.html5 ~/.pandoc/templates/.
-SLIDE_TITLE=20150217-shibuya-rb-saddler
+SLIDE_TITLE=20150327-php-cs-php-study
 ~/.cabal/bin/pandoc --template=html5github -f markdown -t html5 index.md -s -o index.html
 ~/.cabal/bin/pandoc -f markdown index.md -o "$SLIDE_TITLE".pdf --latex-engine=lualatex -V lang:english -V documentclass=ltjarticle
