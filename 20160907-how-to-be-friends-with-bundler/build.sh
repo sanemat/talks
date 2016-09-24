03882ecbe65ed9bf706b4bd08dd59eb178eba59b
Sanemat <o.gata.ken@gmail.com>
2016-09-24 15:44:28 +0900

    Update

--- 20160907-how-to-be-friends-with-bundler/build.sh
+++ 20160907-how-to-be-friends-with-bundler/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# cp html5github.html5 ~/.pandoc/templates/.
 SLIDE_TITLE=20160907-how-to-be-friends-with-bundler
-~/.cabal/bin/pandoc --template=html5github -f markdown -t html5 index.md -s -o index.html
-~/.cabal/bin/pandoc -f markdown index.md -o "$SLIDE_TITLE".pdf --latex-engine=lualatex -V lang:english -V documentclass=ltjarticle
+stack exec pandoc -- --template=./html5github.html5 -f markdown -t html5 index.md -s -o index.html
+stack exec pandoc -- -f markdown index.md -o "$SLIDE_TITLE".pdf --latex-engine=lualatex -V lang:english -V documentclass=ltjarticle
